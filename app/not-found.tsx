1909031e0e094d7682fb1e7d1f623e941db01168
thiagomrib <tmarques929@gmail.com>
2025-11-17 13:06:04 -0300

    Add global not-found page

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Página não encontrada</h1>
+      <p>A página solicitada não existe.</p>
+    </div>
+  );
+}
+
