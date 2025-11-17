241e16f2e01286ef794490b2319e3eaa188037ab
thiagomrib <tmarques929@gmail.com>
2025-11-17 16:40:29 -0300

    fix not-found encoding

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,18 +1,9 @@
-// app/not-found.tsx
 export default function NotFound() {
   return (
-    <div
-      style={{
-        minHeight: "100vh",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-        gap: "0.5rem",
-      }}
-    >
-      <h1>Página não encontrada</h1>
-      <p>O recurso que você está procurando não existe.</p>
-    </div>
+    <html>
+      <body>
+        <h1>Page not found</h1>
+      </body>
+    </html>
   );
 }
